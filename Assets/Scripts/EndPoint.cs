36076224f9f8cf9c8a93077cad92adf6b4736905
amilewska <adoryanna@gmail.com>
2023-01-16 16:26:04 +0100

    EndPoint
    change end point to making it compatible with menu levels

--- Assets/Scripts/EndPoint.cs
+++ Assets/Scripts/EndPoint.cs
@@ -5,11 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class EndPoint : MonoBehaviour
 {
+    
+    public int currentScene;
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        GameManager.instance.levelNumber = currentScene;
+        GameManager.instance.SaveLevelNumber();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
+            SceneManager.LoadScene(currentScene + 1);
+
+            if(currentScene > PlayerPrefs.GetInt("levelAt"))
+            {
+                PlayerPrefs.SetInt("levelAt", currentScene);
+            }
+
+            if (currentScene > GameManager.instance.levelNumber)
+            {
+                GameManager.instance.levelNumber = currentScene;
+                GameManager.instance.SaveLevelNumber();
+
+            }
+
+            
         }
     }
 }
